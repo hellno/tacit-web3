99646cdc18f0f701277641010b02a732e3894844
hellno <holgerniessner@me.com>
2022-05-14 12:57:31 +0200

    forge install: openzeppelin-contracts

--- src/TaskPortal.sol
+++ src/TaskPortal.sol
@@ -29,24 +29,22 @@ contract TaskPortal {
 
     mapping(bytes32 => Node) nodes;
 
-    Task[] public tasks;
-
-    event NewTaskCreated(Task task);
+    event NewNodeCreated(Node node);
 
     constructor() {
         console.log("Created TaskPortal");
     }
 
 
-    function get(bytes32 _name, bytes32 _parent) public view returns (bytes32, bytes32, bytes32, bytes32[]) {
-        Node storage node = nodes[keccak256(_parent, _name)];
+    function get(bytes32 _name, bytes32 _parent) public view returns (bytes32, bytes32, bytes32, bytes32[] memory) {
+        Node storage node = nodes[keccak256(abi.encodePacked(_parent, _name))];
         return (node.name, node.parent, node.data, node.nodes);
     }
 
 
     function addNode(bytes32 _name, bytes32 _parent, bytes32 _data, address _creator, NodeType _nodeType) private {
         require(_name.length > 0);
-        bytes32 path = keccak256(_parent, _name);
+        bytes32 path = keccak256(abi.encodePacked(_parent, _name));
         nodes[path] = Node({
         name : _name,
         creator: _creator,
@@ -56,28 +54,26 @@ contract TaskPortal {
         nodes : new bytes32[](0)
         });
         nodes[_parent].nodes.push(path);
+
+        emit NewNodeCreated(nodes[path]);
     }
 
 
     function addTask(string memory title, string memory description) public {
-        // todo: refactor to create node with action 'CreateTask'
-        //        tasks.push(Task(title, description));
-        bytes32 name = keccak256(title);
-        addNode(name, "", keccak256(description), msg.sender, NodeType.Task);
-
-        emit NewTaskCreated(name);
+        bytes32 name = keccak256(abi.encode(title));
+        addNode(name, "", keccak256(abi.encode(description)), msg.sender, NodeType.Task);
     }
 
 
-    function addShare() public returns (bytes32) {
-        // todo: create node with action = 'Share'
-        bytes32 newNode;
-        return newNode;
-    }
-
-    function addSolution(string memory content) public returns (bytes32) {
-        // todo: create node with action = 'Solve'
-        bytes32 newNode;
-        return newNode;
-    }
+//    function addShare() public returns (bytes32) {
+//        // todo: create node with action = 'Share'
+//        bytes32 newNode;
+//        return newNode;
+//    }
+//
+//    function addSolution(string memory content) public returns (bytes32) {
+//        // todo: create node with action = 'Solve'
+//        bytes32 newNode;
+//        return newNode;
+//    }
 }
