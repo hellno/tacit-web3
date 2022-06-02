b9a26760a64dee675a073995c1da5fc76e67ceeb
hellno <holgerniessner@me.com>
2022-06-02 16:23:46 +0200

    add analytics

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,8 @@ module.exports = {
     SITE: process.env.SITE || process.env.VERCEL_URL,
     USE_LOCAL_NODE: process.env.USE_LOCAL_NODE,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    HEAP_ANALYTICS_ID: process.env.HEAP_ANALYTICS_ID
   },
   images: {
     domains: ['images.unsplash.com']
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,10 +7,36 @@ function MyApp ({
   Component,
   pageProps
 }: AppProps) {
+  const renderAnalyticsScripts = () => {
+    return (
+      <>
+        <script async src={'https://scripts.simpleanalyticscdn.com/latest.js'} />
+        <script async dangerouslySetInnerHTML={{
+          __html: `
+    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+      heap.load("${process.env.HEAP_ANALYTICS_ID}");
+   `
+        }} />
+        <script async dangerouslySetInnerHTML={{
+          __html: `(function(s,q,e,a,u,k,y){
+          s._sqSettings={site_id:'de74bb86-d7ef-4461-afaa-d5eb9264b53c'};
+          u=q.getElementsByTagName('head')[0];
+          k=q.createElement('script');
+          k.src=e+s._sqSettings.site_id;
+          u.appendChild(k);
+        })(window,document,'https://cdn.squeaky.ai/g/0.4.0/script.js?');
+        `
+        }} />
+      </>
+    )
+  }
+  const isProd = process.env.NODE_ENV === 'production'
+
   return <AppContextProvider>
     <BannerComponent
       title={'This is an early alpha release on Goerli Testnet - heavy work on UI and Smart Contracts in progress'} />
     <Component {...pageProps} />
+    {isProd && renderAnalyticsScripts()}
   </AppContextProvider>
 }
 
