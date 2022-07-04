457ffc0d1bde49f5b4d4520fe5906d15d8670304
hellno <holgerniessner@me.com>
2022-07-04 18:04:21 +0200

    use remote image server

--- frontend/pages/share/[shareId].tsx
+++ frontend/pages/share/[shareId].tsx
@@ -619,8 +619,10 @@ export default function SharePage ({ shareObject }) {
     const url = getSiteUrl()
     const imageTitle = shareObject.title
     const imageBountyStr = getBountyDescription()
-    const imageUrl = encodeURI(`${url}/api/og-image?title=${escape(imageTitle)}&bounty=${escape(imageBountyStr)}`)
-    console.log('imageUrl', imageUrl)
+    const ogImageUrl = process.env.OG_IMAGE_URL
+    const isProd = process.env.NODE_ENV === 'production'
+    const imageUrl = isProd && ogImageUrl && encodeURI(`${ogImageUrl}api/og-image?title=${escape(imageTitle)}&bounty=${escape(imageBountyStr)}`)
+
     return (
       <Head>
         {/* <title>{title}</title> */}
@@ -633,16 +635,18 @@ export default function SharePage ({ shareObject }) {
         {/* <meta property="og:title" key="ogtitle" content={title} /> */}
         {/* <meta property="og:description" key="ogdesc" content={description} /> */}
         // image properties
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:width" content="1200" />
+        {imageUrl && (<>
+          <meta property="og:image" content={imageUrl} />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:image:width" content="1200" />
+        </>)}
         // twitter properties
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content={process.env.SITE} />
         <meta property="twitter:url" content={url} />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={imageUrl} />
+        {imageUrl && (<meta name="twitter:image" content={imageUrl} />)}
       </Head>
     )
   }
